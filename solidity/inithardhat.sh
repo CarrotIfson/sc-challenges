fead2d6c3302567e13d62d2077fa994699612730
CarrotIfson <didacysusana@gmail.com>
2022-06-26 12:45:47 +0200

    initial commit for solidity challenges

---
+++ solidity/inithardhat.sh
@@ -0,0 +1,5 @@
+yarn init -y
+yarn add --dev hardhat
+yarn add --dev @uniswap/v3-periphery
+yarn hardhat -y
+wget https://raw.githubusercontent.com/NomicFoundation/hardhat/master/.gitignore -O .gitignore
